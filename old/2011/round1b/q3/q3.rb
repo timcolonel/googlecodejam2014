e74c38408538c9af033b50dae3e0cdb9aa55a5ae
Timothee Guerin <timothee.guerin@outlook.com>
2014-04-21 13:34:02 -0400

    more practice

--- old/2011/round1b/q3/q3.rb
+++ old/2011/round1b/q3/q3.rb
@@ -4,27 +4,52 @@ Bundler.require(:default)
 
 require 'prime'
 
-file = File.new('old/2013/round1a/q3/q3data.txt', 'r')
-
-def get_factors(value, n, m)
-  combinations = []
-  (1...n).each do |k|
-    combinations += (2..m).to_a.repeated_combination(k).to_a
-  end
-  factors = combinations.select do |c|
-    c.inject(:*) == value
-  end
-  factors
-end
+file = File.new('old/2011/round1b/q3/q3data.txt', 'r')
 
 file.gets.to_i.times do |i|
-  r, n, m, k = file.gets.split(' ').map(&:to_i)
-  puts "Case \##{i+1}:"
-  r.times do |j|
-    results = file.gets.split(' ').map(&:to_i)
-    factors = results.map { |x| get_factors(x, k, m) }
-    factors.each_with_index do |f, k|
-      puts 'F: '+ results[k].to_s + ' -- ' + f.to_s
+  n, m = file.gets.split(' ').map(&:to_i)
+  u = file.gets.split(' ').map(&:to_i)
+  v = file.gets.split(' ').map(&:to_i)
+
+
+  rooms = [(1..n).to_a]
+
+  m.times.each do |edge|
+    room = rooms.select { |x| x.include?(u[edge]) and x.include?(v[edge]) }.first
+    rooms.delete(room)
+    new_room = (u[edge]..v[edge]).to_a
+    rooms << new_room
+    rooms << (room.first..u[edge]).to_a+(v[edge]..room.last).to_a
+  end
+
+
+  rooms.sort! do |x, y|
+    x.size <=> y.size
+  end
+
+  pillards = {}
+
+  colors = (1..rooms.first.size).to_a
+  rooms.each do |room|
+    color_missing = colors.clone
+    room.select { |x| not pillards[x].nil? }.each do |pillard|
+      color_missing.delete(pillards[pillard])
+    end
+    room.select { |x| pillards[x].nil? }.each do |pillard|
+      if color_missing.size == 0
+        pillards[pillard] = colors.first
+      else
+        pillards[pillard] = color_missing.pop
+      end
+
     end
   end
+
+
+  puts "Case ##{i+1}: #{rooms.first.size}"
+  results = []
+  (1..n).each do |pillard|
+    results << pillards[pillard]
+  end
+  puts results.join(' ')
 end
