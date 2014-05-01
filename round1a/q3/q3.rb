4650c0e073ecc4b9b861dc5d74faf340a375b52f
Timothee Guerin <timothee.guerin@outlook.com>
2014-05-01 11:18:04 -0400

    2014 r1a q3

--- round1a/q3/q3.rb
+++ round1a/q3/q3.rb
@@ -4,9 +4,17 @@ Bundler.require(:default)
 
 file = File.new('q3data.txt', 'r')
 
-n = file.gets.to_i
-n.times.each do |i|
+file.gets.to_i.times.each do |cas|
 
+  n= file.gets.to_i
+  a = file.gets.split.map(&:to_i)
+  v = 0
+  n.times.each do |i|
+    v += 1 if a[i] >= i
+  end
+
+  good = (v <= 515*n/1000)
+  puts "Case ##{cas+1}: #{good ? 'GOOD' : 'BAD'}"
 end
 
 file.close
