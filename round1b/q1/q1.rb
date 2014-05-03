77f9966ff18a4058fd5de8c9546398c814a1dc93
Timothee Guerin <timothee.guerin@outlook.com>
2014-05-03 20:52:19 +0200

    fail

--- round1b/q1/q1.rb
+++ round1b/q1/q1.rb
@@ -5,8 +5,79 @@ Bundler.require(:default)
 file = File.new('q1data.txt', 'r')
 
 
-file.gets.to_i.times.each do |i|
+def compute_cost(str, str_t)
+  return 0 if str == str_t
 
+  cost = 0
+  i = 0
+  until str[i].nil? and str_t[i].nil? or str == str_t
+    if str[i]!=str_t[i]
+      # puts "d #{str[i]} - #{str_t[i]}"
+      if str_t[i-1] == str_t[i] and str_t[i+1] == str[i]
+        cost += 1
+        str_t = str_t[0...i].to_s + str_t[i+1..-1].to_s
+        # puts "Removed #{str} -- #{str_t}"
+      elsif (str[i-1] == str_t[i-1] and str[i-1] == str[i] and not i-1 < 0) or (str[i+1] == str_t[i+1] and str[i+1] == str[i] and not i >= str_t.size)
+        cost += 1
+        str_t = str_t[0...i].to_s + str[i].to_s + str_t[i..-1].to_s
+        # puts "Added #{str} -- #{str_t}"
+      else
+        # puts 'ab'
+        return nil
+      end
+    end
+    i += 1
+  end
+  cost
+end
+
+file.gets.to_i.times.each do |cas|
+  n = file.gets.to_i
+
+  strs = []
+  size = 0
+  n.times.each do
+    strs << file.gets.chomp
+    size = strs.size if strs.size > size
+  end
+  best = nil
+  i = 0
+
+  best = 0
+
+  stop = false
+  while i < size
+    same = true
+    strs.each do |s|
+      if strs.first[i] != s[i]
+        same = false
+      end
+    end
+    unless same
+      broke = false
+      puts "Not same #{strs}"
+      strs.each_with_index do |str, j|
+        if str[i] == str[i+1] or str[i] == str[i-1]
+          strs[j] = str[0...i].to_s + str[i+1..-1].to_s
+          puts strs[j]
+          best += 1
+          i -= 1
+          broke = true
+          break
+        end
+
+        unless broke
+          stop = true
+          break
+        end
+      end
+    end
+
+    break if stop
+    i += 1
+  end
+
+  puts "Case ##{cas+1}: #{best}" unless stop
 end
 
 
--- round1b/q2/q2.rb
+++ round1b/q2/q2.rb
@@ -6,7 +6,30 @@ file = File.new('q2data.txt', 'r')
 
 
 file.gets.to_i.times.each do |i|
+  a, b, k = file.gets.split.map(&:to_i)
+  ba = a.to_s(2)
+  bb = b.to_s(2)
+  bk = k.to_s(2)
 
+  c = 0
+  (k...a).each do |a0|
+    (k...b).each do |b0|
+      if a0 & b0 < k
+        c += 1
+      elsif a0 & b0 > 2*k
+        break
+      end
+    end
+
+    if a0 & (2*k) >= k
+      break
+    end
+  end
+  c+= k*k + k*(b-k+a-k)
+  if c < 0
+    c = 0
+  end
+  puts "Case ##{i+1}: #{c}"
 end
 
 
